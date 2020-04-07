6d9d9d8e5177cec057dca03ee641ecc612de843f
Manav Narang <36950613+manavnarang@users.noreply.github.com>
2020-04-07 17:13:32 +0530

    Create LetsReview.py

---
+++ Day 6: Let's Review/LetsReview.py
@@ -0,0 +1,13 @@
+T=int(input( ))
+
+for i in range(0,T):
+    string=input( )
+    for j in range(0,len(string)):
+        if(j%2==0):
+            print(string[j], end='')
+    print(' ',end='')
+
+    for j in range(0, len(string)):
+        if(j%2!=0):
+            print(string[j],end='')
+    print("")
